1ba59848e6e0aed933999913a616e6e97db6fc8f
Daniel Høier Øhrgaard <daniel@yoke.dk>
2018-01-27 00:13:00 +0100

    Add some model specs

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Track, type: :model do
+  it { should belong_to(:playlist) }
   it { should validate_presence_of(:title) }
 end
--- spec/requests/tracks_spec.rb
+++ spec/requests/tracks_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Tracks", type: :request do
   let(:playlist) { create :playlist }
 
-  describe "GET /tracks" do
+  describe "GET /playlists/:playlist_id/tracks" do
     it "works! (now write some real specs)" do
       get playlist_tracks_path(playlist)
       expect(response).to have_http_status(200)
