e645cf116f227ccedfb51ec770c3c84f9013c7dd
Daniel Høier Øhrgaard <daniel@yoke.dk>
2018-01-30 14:12:58 +0100

    Make interval image description a simple text field

--- spec/views/interval_images/edit.html.erb_spec.rb
+++ spec/views/interval_images/edit.html.erb_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe "interval_images/edit", type: :view do
     render
 
     assert_select "form[action=?][method=?]", interval_image_path(@interval_image), "post" do
-      assert_select "textarea.form-control[name=?][required]", "interval_image[description_da]"
-      assert_select "textarea.form-control[name=?]", "interval_image[description_en]"
+      assert_select "input.form-control[name=?][required]", "interval_image[description_da]"
+      assert_select "input.form-control[name=?]", "interval_image[description_en]"
       assert_select "input.form-control[name=?][required]", "interval_image[source_da]"
       assert_select "input.form-control[name=?]", "interval_image[source_en]"
       assert_select "input[name=?][accept=?]", "interval_image[file]", "image/jpeg,image/pjpeg,image/png"
